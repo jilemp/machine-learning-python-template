560357c89fe13d364e3beb00cc09d41c98aedb8e
José Ignacio Ibarra Lemp <jiibarra@uc.cl>
2025-02-21 01:15:20 +0000

    streamlit update

--- src/ml_webapp_using_streamlit_tutorial.py
+++ src/ml_webapp_using_streamlit_tutorial.py
@@ -55,7 +55,35 @@ def main():
     data_range = st.slider("Seleccione el rango que quiere evaluar", 0, 100, (10,90))
     x_display = X_test[data_range[0]:data_range[1]]
     y_display = y_test[data_range[0]:data_range[1]]
-    y_pred_display = y_pred
+    y_pred_display = y_pred[data_range[0]:data_range[1]]
+
+    #visualizacion
+    fig, ax = plt.subplots()
+    if opcion == "Dispersion":
+        ax.scatter(x_display, y_display)
+        plt.title("Grafico de Dispersion")
+    else:
+        ax.plot(x_display, y_pred_display)
+        plt.title("Grafico de Linea")
+    st.pyplot(fig)
+
+    opcion_2 = st.multiselect('Informacion Adicional', ['Informe General', 'Prediccion'])
+
+    if 'Informe General' in opcion_2:
+        st.write('Aca deberian estar los datos!')
+        st.write(pd.DataFrame({'X Test': X_test.flatten(), 'y Test': y_test.flatten(), 'y Predict':y_pred.flatten()}))
+
+    if 'Prediccion' in opcion_2:
+        st.write('Aca tambien deberian haber datos!')
+        for i in range(len(x_display)):
+            st.write(f'x = {x_display[i][0]:.2f} prediccion y = {y_pred_display[i][0]:.2f}')
+
+    #recibir informacion de usuario
+    st.header('Ingrese su valor de X')
+    x_ingresado = st.number_input('Numero a predecir')
+    if st.button('Calcular'):
+        prediccion_x = model.predict([[x_ingresado]])[0][0]
+        st.write(f'La preddiccion para {x_ingresado} es {prediccion_x}')
 
 if __name__ == '__main__':
     main()
